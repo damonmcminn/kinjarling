5620643e75dced14b61af1db3e67c6643764a964
Damon McMinn <damonrmcminn@gmail.com>
2015-06-03 12:30:04 +0100

    Increase length of API key

--- es6/src/Service/generateId.js
+++ es6/src/Service/generateId.js
@@ -2,7 +2,7 @@ import {randomBytes} from 'crypto';
 
 function generateId() {
   // URL safe base64: necessary as id's are used as path identifiers
-  return randomBytes(12).toString('base64').replace(/\+/g, '-').replace(/\//g, '_');
+  return randomBytes(18).toString('base64').replace(/\+/g, '-').replace(/\//g, '_');
 }
 
 export default generateId;
